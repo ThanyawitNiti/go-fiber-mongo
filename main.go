0f017cf6ebb2a6fc4aee6554fbc5b90077bdf746
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-10-09 16:43:29 +0700

    create : get employee

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	//create id for every record
@@ -26,10 +27,10 @@ const mongoURI = "mongodb://localhost:27017" + dbName
 // const mongoURI = "mongodb+srv://thanyawit:Q5rvmuP6FYHhCRaX@cluster0.zff25.mongodb.net/golang_db?retryWrites=true&w=majority&appName=Cluster0" + dbName
 
 type Employee struct {
-	ID     string  `json:"id" bson:"employeeId"`
-	Name   string  `json:"name" bson:"name"`
-	Salary float64 `json:"salary" bson:"salary"`
-	Age    float64 `json:"age" bson:"age"`
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
 }
 
 func Connect() error { // connect golang to db
@@ -46,7 +47,7 @@ func Connect() error { // connect golang to db
 
 	mg = MongoInstance{
 		Client: client,
-		Db:     db,
+		Db:     db, //connect to db
 	}
 	return nil
 }
@@ -59,6 +60,19 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/employee", func(c *fiber.Ctx) error { //c for accessing res and req
+
+		query := bson.D{{}}
+
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		var employees []Employee = make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), &employees); err != nil { //convert type of data
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(employees)
 
 	})
 	app.Post("/employee")

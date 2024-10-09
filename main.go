ea14b38bfe0d54746be37f8dd82f22677ac23add
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-10-09 17:20:02 +0700

    create : put and del function

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	//create id for every record
@@ -102,6 +103,69 @@ func main() {
 		return c.Status(201).JSON(createdEmployee)
 
 	})
-	app.Put("/employee/:id")
-	app.Delete("/employee/:id")
+
+	app.Put("/employee/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+
+		employeeID, err := primitive.ObjectIDFromHex(idParam)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		update := bson.D{
+			{Key: "$set",
+				Value: bson.D{
+					{key: "name", Value: employee.Name},
+					{key: "age", Value: employee.Age},
+					{key: "salary", Value: employee.Salary},
+				},
+			},
+		}
+
+		mg.Db.Collection("employee").FindOneAndUpdate(c.Context(), query, update).Err()
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.SendStatus(400)
+			}
+			return c.SendStatus(500)
+		}
+		employee.ID = idParam
+
+		return c.SendStatus(200).JSON(employee)
+	})
+
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
+
+		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+
+		deleteResult, err := mg.Db.Collection("employee").DeleteOne(c.Context(), query)
+
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		if deleteResult.DeletedCount < 1 { // nothing got delete
+			return c.SendStatus(404)
+		}
+
+		return c.SendStatus(200).JSON("recoed deleted")
+	})
+
+	app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
